0f7533b442c603ff32b774668f8f77dcd62a5cf9
andreadldev <andrea.delisi2@gmail.com>
2022-11-07 16:02:17 +0100

    Crea array di oggetti

---
+++ script.js
@@ -0,0 +1,37 @@
+const team = [
+    {
+    'name': 'Wayne Barnett',
+    'position': 'Founder & Ceo',
+    'image': 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+    'name': 'Angela Caroll',
+    'position': 'Chief Editor',
+    'image': 'angela-caroll-chief-editor.jpg'
+    },
+
+    {
+    'name': 'Walter Gordon',
+    'position': 'Office Manager',
+    'image': 'walter-gordon-office-manager.jpg'
+    },
+
+    {
+    'name': 'Angela Lopez',
+    'position': 'Social Media Manager',
+    'image': 'angela-lopez-social-media-manager.jpg'
+    },
+
+    {
+    'name': 'Scott Estrada',
+    'position': 'Developer',
+    'image': 'scott-estrada-developer.jpg'
+    },
+
+    {
+    'name': 'Barbara Ramos',
+    'position': 'Graphic Designer',
+    'image': 'barbara-ramos-graphic-designer.jpg'
+    }
+]
